781bdb327c296a59797286d01a62e06afdf81ca8
piotr-aueternum <piotr.aueternum@gmail.com>
2017-05-28 10:45:31 +0200

    fixed middleware

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import logger from 'redux-logger';
 import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
+import logger from 'redux-logger';
 import App from './components/App';
 import reducer from './reducers/index';
 import mySaga from './sagas';
 
-const middleware = process.env.DEVELOPMENT ? logger : undefined;
 const sagaMiddleware = createSagaMiddleware();
+const middleware = process.env.DEVELOPMENT
+  ? applyMiddleware(sagaMiddleware, logger)
+  : applyMiddleware(sagaMiddleware);
 const store = createStore(
   reducer,
-  applyMiddleware(sagaMiddleware, middleware),
+  middleware,
 );
 sagaMiddleware.run(mySaga);
 
